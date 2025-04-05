175df53000d02839c11571d604260926140675d1
Vaisakhkrishnan K <82216754+kvaisakhkrishnan@users.noreply.github.com>
2025-04-05 15:06:40 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "mongo-swift-driver",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v12),
         .macOS(.v10_14)
     ],
     products: [
